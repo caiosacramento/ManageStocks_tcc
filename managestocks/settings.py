32db587717e2f0b639c2adc1aa032fdff30eb11d
Caio Sacramento <caiosacramento@outlook.com>
2023-11-11 21:27:30 -0300

    teste

--- managestocks/settings.py
+++ managestocks/settings.py
@@ -150,9 +150,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_DIRS = [
-   #os.path.join(BASE_DIR, 'templates/static')
-#]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'templates/static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
